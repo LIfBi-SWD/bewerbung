76b964eefe1229906ba0fe005886e4afe4d99a21
Kareem Ali <kareem.ali.rahman@gmail.com>
2022-08-26 19:16:05 +0200

    finish test

--- lifbi.sanduhr.logic/Sanduhr.cs
+++ lifbi.sanduhr.logic/Sanduhr.cs
@@ -4,8 +4,29 @@ namespace lifbi.sanduhr.logic
 {
     public class Sanduhr
     {
-        public string Print(int width){
-            return "*";
+        public string Print(int width)
+        {
+            if (width == 1) throw new ArgumentOutOfRangeException();
+            string res = "";
+            int i;
+            for (i = width; i > 0; i -= 2)
+                res += RepeatedStarWithSpaces(i, (width - i) / 2) + Environment.NewLine;
+            for (i += 4; i <= width; i += 2)
+            {
+                res += RepeatedStarWithSpaces(i, (width - i) / 2);
+                if (i + 2 <= width) res += Environment.NewLine;
+            }
+            return res;
+        }
+
+        private string RepeatedStarWithSpaces(int width, int spaces)
+        {
+            string res = "";
+            for (int i = 0; i < spaces; i++)
+                res += " ";
+            for (int i = 0; i < width; i++)
+                res += "*";
+            return res;
         }
     }
 }
