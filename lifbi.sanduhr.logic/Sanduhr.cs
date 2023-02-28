68c4534359dba9b438699f34b0903bc87e67f024
viki-bee <125653464+viki-bee@users.noreply.github.com>
2023-02-28 21:12:41 +0100

    Update Sanduhr.cs
    implement logic for Sanduhr.cs // tests running bug-free

--- lifbi.sanduhr.logic/Sanduhr.cs
+++ lifbi.sanduhr.logic/Sanduhr.cs
@@ -4,8 +4,49 @@ namespace lifbi.sanduhr.logic
 {
     public class Sanduhr
     {
-        public string Print(int width){
-            return "*";
+        // void Main()
+        // {
+        //     Console.WriteLine(Print(3)); // example usage
+        //     Console.WriteLine(Print(4)); // example usage
+        //     Console.WriteLine(Print(5)); // example usage
+        //     Console.WriteLine(Print(99)); // example usage
+        // }
+
+        public string Print(int width)
+        {
+            if (width < 3)
+            {
+                throw new ArgumentOutOfRangeException("width", "Width must be greater than or equal to 3.");
+            }
+
+            int halfWidth = (width + 1) / 2;
+            string result = "";
+
+            for (int i = 1; i <= halfWidth; i++)
+            {
+                result += GetSpaces(i - 1);
+                result += GetStars(width - 2 * (i - 1));
+                result += "\n";
+            }
+
+            for (int j = halfWidth - 1; j >= 1; j--)
+            {               
+                result += GetSpaces(j - 1);
+                result += GetStars(width - 2 * (j - 1));
+                result += "\n";              
+            }
+
+            return result;
+        }
+
+        private static string GetSpaces(int count)
+        {
+            return new String(' ', count);
+        }
+
+        private static string GetStars(int count)
+        {
+            return new String('*', count);
         }
     }
 }
